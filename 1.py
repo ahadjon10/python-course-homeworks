0660d9a4d422575353b087c75f86b8455ed1a475
Axadjon <men2003yilman@gmail.com>
2023-03-17 15:05:58 +0500

    1-dars

---
+++ 1.py
@@ -0,0 +1,14 @@
+print('"Nexia", "Tiko", \'Damas\' ko\'rganlar qilar havas')
+#5 ning 4-darajasini toping
+print('5 ning 4-darajasi', 5**4)
+#22 ni 4 ga bo'lganda qancha qoldiq qoladi?
+print('22 ni 4 ga bo\'lganda qancha qoldiq', 22%4)
+#Tomonlari 125 ga teng kvadratning yuzi va perimetrini toping
+print('Tomonlari 125 ga teng kvadratning yuzi',125**2, '\n va perimetri', 125*4 )
+#Diametri 12 ga teng bo'lgan doiraning yuzini toping (
+#π
+pi=3.14
+# deb oling)
+print("Diametri 12 ga teng bo\'lgan doiraning yuzi", 12*pi)
+#Katetlari 6 va 7 bo'lgan to'g'ri burchakli uchburchakning gipotenuzasini toping (Pifagor teoremasidan foydalaning)
+print('Katetlari 6 va 7 bo\'lgan to\'g\'ri burchakli uchburchakning gipotenuzasi', 6**2+7**2)
